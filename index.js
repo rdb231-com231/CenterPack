d50976ab3d484047df1ad49845650a3ec81a6333
Flávio Fiuza <flaviofdbastos@gmail.com>
2022-05-27 19:06:54 -0300

    Add Files

---
+++ index.js
@@ -0,0 +1,43 @@
+export default class Select {
+    constructor(main) {
+        this.main = main
+    };
+
+    send() {
+        return console.log(this.main);
+    };
+
+    static randint(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    randnum(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    static output(msg) {
+        return console.log(msg);
+    }
+};
+
+export function send(msg) {
+    return console.log(msg.toString());
+};
+
+export class Store {
+    data = {};
+    constructor(name) {
+        this.data[name];
+    };
+
+    store(data, name) {
+        this.data[name] = data;
+    };
+
+    remove(data, name) {
+        if (data.name[data]) data.name[data] = null;
+        else return;
+    };
+};
+
+
